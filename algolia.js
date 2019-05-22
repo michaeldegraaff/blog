15b1898306887bbf6179db0a63224c7e3643619f
Joost <joost@meijles.com>
2019-05-22 20:40:40 +0200

    Fix Algolia push using node-fetch

--- algolia.js
+++ algolia.js
@@ -1,30 +1,25 @@
 // Develop & build using netlify-lambda
-const algoliasearch = require('algoliasearch');
-const axios = require('axios');
+import algoliasearch from 'algoliasearch';
+import fetch from 'node-fetch';
 
 const algoliaApp = '050Q9I7JU4';
 const algoliaIndex = 'joost.meijles.com';
 
-exports.handler = function(event, context, callback) {
+exports.handler = async (event, context, callback) => {
+    // Fetch data
+    const url = `${process.env.URL}/algolia.json`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // Init Algolia
     const client = algoliasearch(algoliaApp, process.env.ALGOLIA_ADMIN_KEY);
     const index = client.initIndex(algoliaIndex);
 
-    axios.get(`${process.env.URL}/algolia.json`)
-        .then(res => res.json())
-        .then(json => {
-            index.addObjects(json, function(err, content) {
-                if (err) {
-                    callback(null, {
-                        statusCode: 500,
-                        body: err
-                    });
-                }
-                else {
-                    callback(null, {
-                        statusCode: 200,
-                        body: `Completed task: ${content.taskID}`
-                    });
-                }
-            })
-        });
+    // Add documents to Algolia
+    const res = await index.addObjects(data);
+
+    return {
+        statusCode: 200,
+        body: `Completed task ${res.taskID}`
+    };
 }
